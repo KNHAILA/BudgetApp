0419b1e4197fa2fb5b1b28b7b216733d2abdca65
unknown <nhailakaoutar@gmail.com>
2020-06-23 03:23:50 +0100

    Fist Commit

---
+++ src/app/edit-item-model/edit-item-model.component.ts
@@ -0,0 +1,22 @@
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {BudgetItem} from "../../shared/models/budget-item.model";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-edit-item-model',
+  templateUrl: './edit-item-model.component.html',
+  styleUrls: ['./edit-item-model.component.scss']
+})
+export class EditItemModelComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<EditItemModelComponent>,
+    @Inject(MAT_DIALOG_DATA) public item: BudgetItem){}
+
+  ngOnInit(): void {
+  }
+
+  onSubmitted(updatedItem: BudgetItem){
+    this.dialogRef.close(updatedItem);
+  }
+
+}
